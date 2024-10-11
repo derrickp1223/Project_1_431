b62c04743f1fd7d58b2591dda335a87f296d5242
Joaquin Nietes <nietesj@sunypoly.edu>
2024-10-11 15:28:59 -0400

    Created getRandElement function

--- Project1/_build/default/bin/app.ml
+++ Project1/_build/default/bin/app.ml
@@ -1,1 +1,10 @@
 let () = print_endline "Hello, World!"
+
+let testList = ["lions"; "tiger"; "bears"]
+
+let rec getElement list index =
+  match list with
+  | [] -> None
+  | hd :: tl -> if index = 0 then Some hd else getElement tl (index - 1)
+;;
+getElement testList 0;;
--- Project1/bin/app.ml
+++ Project1/bin/app.ml
@@ -1,1 +1,10 @@
 let () = print_endline "Hello, World!"
+
+let testList = ["lions"; "tiger"; "bears";"wasps";"crabs";"snake";"rhino"]
+
+let getRandElement list =
+  let randIndex = Random.int (List.length list) in
+  List.nth list randIndex
+;;
+
+getRandElement testList;;
