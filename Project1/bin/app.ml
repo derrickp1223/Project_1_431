8bbd4d84da8587bc9ff8eebbe1521f69114da18b
Joaquin Nietes <nietesj@sunypoly.edu>
2024-11-05 09:57:45 -0500

    Modified updateLayout function to modify an existing layout instead of creating a new layout with new values

--- Project1/bin/app.ml
+++ Project1/bin/app.ml
@@ -17,15 +17,15 @@ let makeBoxWidgit color =
 let makeBoxLayout color text =
   L.superpose ~w:50 ~h:50 ~center:true 
   [
-    L.resident ((makeBoxWidgit color) ());
-    L.resident (W.label text ~size:48 ~fg: Draw.(opaque(251,251,251)));
+    L.resident ~w:50 ((makeBoxWidgit color) ());
+    L.resident ~w:50 (W.label text ~size:48 ~fg: Draw.(opaque(251,251,251)));
   ]
 
-
-let rec getWidgetsFromLayout layout =
-  match L.get_content layout with
-  | Resident widget -> [widget]
-  | Rooms rooms -> List.flatten (List.map getWidgetsFromLayout rooms)
+(* Function to extract W.labels from a layout *)
+  let rec getWidgetsWithText layout =
+    match L.get_content layout with
+    | Resident widget -> if W.get_text widget <> "" then [widget] else []
+    | Rooms rooms -> List.flatten (List.map getWidgetsWithText rooms)
 
 (* Function to extract text from widgets *)
 
@@ -39,14 +39,6 @@ let grey = (128,128,128)
 
 let makeRow colorList charList =
   L.flat ~scale_content:false ~align:Center (List.map2 (fun color char -> makeBoxLayout color char) colorList charList) 
-
-let testRow = makeRow [(1,154,1);(255,196,37);(1,154,1);(255,196,37);(1,154,1)] ["A"; "S"; "D"; "F"; "G"]
-
-let getTextStringsFromWidgets widgets =
-  List.filter_map (fun w -> Some (W.get_text w)) widgets
-
-let stringsFromTestRow = getTextStringsFromWidgets (getWidgetsFromLayout testRow)
-
 
 (* Global list reference to store the text *)
 let keysPressed = ref []
@@ -63,34 +55,44 @@ let getPressedButton () =
 
 let clickKeyboard = 
   let firstRow = L.flat ~align:Center ~scale_content:true (List.map (fun b -> L.resident (makeButton b)) ["Q"; "W"; "E"; "R"; "T"; "Y"; "U"; "I"; "O"; "P";"Back"]) in
-  let secondRow = L.flat ~align:Center ~scale_content:true (List.map (fun b -> L.resident (makeButton b)) ["A"; "S"; "D"; "F"; "G"; "H"; "J"; "K"; "L"]) in
+  let secondRow = L.flat ~align:Center ~scale_content:true (List.map (fun b -> L.resident (makeButton b)) ["A"; "S"; "D"; "F"; "G"; "H"; "J"; "K"; "L"; "Enter"]) in
   let thirdRow = L.flat ~align:Center ~scale_content:true (List.map (fun b -> L.resident (makeButton b)) ["Z"; "X"; "C"; "V"; "B"; "N"; "M"]) in  
   L.tower ~align:Center ~background:(L.color_bg Draw.(opaque(find_color "dark_grey"))) [firstRow;secondRow;thirdRow]
-  let updateLayout layout =
+
+  let updateText layout =
     getPressedButton ()
     |> Option.iter (fun x -> 
         if x = "Back" then (
           if List.length !keysPressed > 0 then (
             keysPressed := List.rev (List.tl (List.rev !keysPressed));
             print_endline (String.concat ", " !keysPressed);
-            let dis = L.flat ~align:Center ~scale_content:false (List.map (fun x -> makeBoxLayout grey x) !keysPressed) in
-            L.set_rooms ~sync:false layout [dis];
+            let widgets = getWidgetsWithText layout in
+            List.iteri (fun i w -> 
+              if i < List.length !keysPressed then
+                W.set_text w (List.nth !keysPressed i)
+              else
+                W.set_text w " "
+            ) widgets;
           )
         ) else if List.length !keysPressed < 5 then (
           keysPressed := !keysPressed @ [x];
           print_endline (String.concat ", " !keysPressed);
-          let dis = L.flat ~align:Center ~scale_content:false (List.map (fun x -> makeBoxLayout grey x) !keysPressed) in
-          L.set_rooms ~sync:false layout [dis];
+          let widgets = getWidgetsWithText layout in
+          List.iteri (fun i w -> 
+            if i < List.length !keysPressed then
+              W.set_text w (List.nth !keysPressed i)
+            else
+              W.set_text w " "
+          ) widgets;
         )
       )
 
 let main () =
-  print_endline (String.concat ", " stringsFromTestRow);
-  let displayLayout = L.superpose ~w:290 ~h:70 ~background:(L.color_bg Draw.(opaque(find_color "grey"))) [] in
+  let displayLayout = L.superpose ~w:290 ~h:70 ~background:(L.color_bg Draw.(opaque(find_color "grey"))) [makeRow [grey;grey;grey;grey;grey] [" ";" ";" ";" ";" "]] in
   let finalLayout = L.tower ~scale_content:false ~name:"Not Wordle" ~align:Center ~background:(L.color_bg Draw.(opaque(find_color "dark_grey"))) [displayLayout;clickKeyboard] in
   let before_display () =
     Some ()
-    |> Option.iter (fun _ -> updateLayout displayLayout) in
+    |> Option.iter (fun _ -> updateText displayLayout) in
   Bogue.run ~before_display (Bogue.of_layout finalLayout)
 
 let () = main ();
