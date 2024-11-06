afb44ef8a11707bd38dca5150aae71df0b680ac2
Joaquin Nietes <nietesj@sunypoly.edu>
2024-11-06 09:41:15 -0500

    Added multiple rows/tries

--- Project1/_build/default/bin/app.ml
+++ Project1/_build/default/bin/app.ml
@@ -2,9 +2,30 @@
 let testList = ["LIONS"; "TIGER"; "BEARS";"WASPS";"CRABS";"SNAKE";"RHINO"]
 
 let getRandElement list =
+  Random.self_init ();  (* Initializes the random generator with a new seed *)
   let randIndex = Random.int (List.length list) in
   List.nth list randIndex
 ;;
+
+(* Read lines from file*)
+let readLinesFromFile filename =
+  let lines = ref [] in
+  let chan = open_in filename in
+  try
+    while true; do
+      lines := input_line chan :: !lines
+    done; !lines
+  with End_of_file ->
+    close_in chan;
+    List.rev !lines ;;
+;;
+
+(* Picks a random line from the file *)
+let getRandomStringFile filename =
+  let lines = readLinesFromFile filename in
+    getRandElement lines
+;;
+
 
 (* Selects a string from our list and converts it to a list of characters *)
 let stringToCharList str =
@@ -16,15 +37,25 @@ let stringListToCharList list =
   List.concat (List.map (fun str -> List.of_seq (String.to_seq str)) list)
 ;;
 
+(* Make the char list uppercase to match our input *)
+let charListToUppercase list =
+  let str = String.of_seq (List.to_seq list) in   (* Convert char list to string *)
+  let upper_str = String.uppercase_ascii str in         (* Convert string to uppercase *)
+  List.of_seq (String.to_seq upper_str) 
+;;
+
 (* Test answer *)
 let testAnswer = stringToCharList (getRandElement testList);;
+let answer = charListToUppercase (stringToCharList (getRandomStringFile "words"));;
 
 (* Prints a list of characters*)
 let printCharList list =
   List.iter (fun c -> Printf.printf "%c " c) list;
   print_endline ("");
 ;;
+
 printCharList testAnswer;;
+printCharList answer;;
 
 
 
@@ -66,6 +97,14 @@ let makeRow colorList charList =
 
 (* Global list reference to store the text *)
 let keysPressed = ref []
+
+(* Global int reference for # of tries *)
+let currentTry = ref 0
+
+let gameOver = ref false
+
+(* Global reference to store the button pressed *)
+
 let pressedButton = ref None
 let getPressedButton () =
   let b = !pressedButton
@@ -84,10 +123,11 @@ let clickKeyboard =
   let thirdRow = L.flat ~align:Center ~scale_content:true (List.map (fun b -> L.resident (makeButton b)) ["Z"; "X"; "C"; "V"; "B"; "N"; "M"]) in  
   L.tower ~align:Center ~background:(L.color_bg Draw.(opaque(find_color "dark_grey"))) [firstRow;secondRow;thirdRow]
 
-let updateText layout =
+let updateText layoutList index =
   getPressedButton ()
   |> Option.iter (fun x -> 
-      if x = "Back" then (
+    let layout = List.nth layoutList index in
+      if x = "Back" && !gameOver == false then (
         if List.length !keysPressed > 0 then (
           (* Removes last character from list *)
           keysPressed := List.rev (List.tl (List.rev !keysPressed));
@@ -114,61 +154,74 @@ let updateText layout =
               W.set_text w " "
         ) widgets;
       ) else if x = "Enter" && List.length !keysPressed = 5 then (
-          (* If the entered string matches our answer returns correct! *)
-          if stringListToCharList !keysPressed = testAnswer then 
-            print_endline ("Correct")
-          (* Else compares each letter and returns true or false for each*)
-          else 
-            print_endline ("Wrong");
-            let comList = stringListToCharList !keysPressed in
-            List.iteri (fun i _ ->
-              if i < List.length comList then 
-                if List.nth comList i = List.nth testAnswer i then
-                  lettersCorrect := !lettersCorrect @ ['t']
-                else if List.mem (List.nth comList i) testAnswer then
-                  lettersCorrect := !lettersCorrect @ ['i']
-                else 
-                  lettersCorrect := !lettersCorrect @ ['f']
-            ) comList;
-            printCharList !lettersCorrect;
+        (* If the entered string matches our answer returns correct! *)
+        if stringListToCharList !keysPressed = testAnswer then 
+          print_endline ("Correct")
+        (* Else compares each letter and returns true or false for each*)
+        else 
+          print_endline ("Wrong");
+          let comList = stringListToCharList !keysPressed in
+          List.iteri (fun i _ ->
+            if i < List.length comList then 
+              if List.nth comList i = List.nth answer i then
+                lettersCorrect := !lettersCorrect @ ['t']
+              else if List.mem (List.nth comList i) answer then
+                lettersCorrect := !lettersCorrect @ ['i']
+              else 
+                lettersCorrect := !lettersCorrect @ ['f']
+          ) comList;
+          printCharList !lettersCorrect;
 
-          
-          (* Updates square colors to green if true or yellow TODO*)
-          let box_widgets = getWidgetsWithoutText layout in
-          List.iteri (fun i _ -> 
-            if i < List.length !lettersCorrect then
-              if List.nth !lettersCorrect i = 't' then
-                (*set color green*)
-                  Box.set_style (W.get_box (List.nth box_widgets i)) Style.(of_bg (opaque_bg (green)))
-              else if List.nth !lettersCorrect i = 'i' then
-                (*set color yellow*)
-                  Box.set_style (W.get_box (List.nth box_widgets i)) Style.(of_bg (opaque_bg (yellow)))
-              else if List.nth !lettersCorrect i = 'f' then
-                (*set color dark_grey*)
-                  Box.set_style (W.get_box (List.nth box_widgets i)) Style.(of_bg (opaque_bg (Draw.find_color "dark_grey")))
-          ) box_widgets;
-          lettersCorrect := []; (* Reset letters correct *)
-          
+        
+        (* Updates square colors to green if true or yellow TODO*)
+        let box_widgets = getWidgetsWithoutText layout in
+        List.iteri (fun i _ -> 
+          if i < List.length !lettersCorrect then
+            if List.nth !lettersCorrect i = 't' then
+              (*set color green*)
+                Box.set_style (W.get_box (List.nth box_widgets i)) Style.(of_bg (opaque_bg (green)))
+            else if List.nth !lettersCorrect i = 'i' then
+              (*set color yellow*)
+                Box.set_style (W.get_box (List.nth box_widgets i)) Style.(of_bg (opaque_bg (yellow)))
+            else if List.nth !lettersCorrect i = 'f' then
+              (*set color dark_grey*)
+                Box.set_style (W.get_box (List.nth box_widgets i)) Style.(of_bg (opaque_bg (Draw.find_color "dark_grey")))
+        ) box_widgets;
+        lettersCorrect := []; (* Reset letters correct *)
+        
+        (* Prints our keysPressed list *)
+        print_endline (String.concat ", " !keysPressed);
+        let widgets = getWidgetsWithText layout in
+        List.iteri (fun i w -> 
+          if i < List.length !keysPressed then
+            W.set_text w (List.nth !keysPressed i)
+          else
+          W.set_text w " "
+        ) widgets;
 
-          (* Prints our keysPressed list *)
-          print_endline (String.concat ", " !keysPressed);
-          let widgets = getWidgetsWithText layout in
-          List.iteri (fun i w -> 
-            if i < List.length !keysPressed then
-              W.set_text w (List.nth !keysPressed i)
-            else
-            W.set_text w " "
-          ) widgets;
+        (* Goes to the next row *)
+        if !currentTry < 5 then (
+          currentTry := !currentTry + 1;
+          (* Resets keysPressed *)  
+          keysPressed := [];
+        ) else (
+          gameOver := true;
+          print_endline ("Game Over");
+        )
       ) 
     )
 
+let makeDisplayLayout name =
+  L.superpose ~name:name ~w:290 ~h:70 ~background:(L.color_bg Draw.(opaque(find_color "grey"))) [makeRow [grey;grey;grey;grey;grey] [" ";" ";" ";" ";" "]]
+
+let displayRows = [makeDisplayLayout "row1";makeDisplayLayout "row2";makeDisplayLayout "row3";makeDisplayLayout "row4";makeDisplayLayout "row5";makeDisplayLayout "row6"]
 
 let main () =
-  let displayLayout = L.superpose ~w:290 ~h:70 ~background:(L.color_bg Draw.(opaque(find_color "grey"))) [makeRow [grey;grey;grey;grey;grey] [" ";" ";" ";" ";" "]] in
-  let finalLayout = L.tower ~scale_content:false ~name:"Not Wordle" ~align:Center ~background:(L.color_bg Draw.(opaque(find_color "dark_grey"))) [displayLayout;clickKeyboard] in
+  let finalLayout = L.tower ~scale_content:false ~name:"Not Wordle" ~align:Center ~background:(L.color_bg Draw.(opaque(find_color "dark_grey"))) 
+  (displayRows @ [clickKeyboard]) in
   let before_display () =
     Some ()
-    |> Option.iter (fun _ -> updateText displayLayout) in
+    |> Option.iter (fun _ -> updateText displayRows !currentTry) in
   Bogue.run ~before_display (Bogue.of_layout finalLayout)
 
 let () = main ();
